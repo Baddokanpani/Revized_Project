c3e2e07a059ad02166b6da039e9d3896affacc42
Baddokanpani <Rico.tru.suave@gmail.com>
2025-03-18 23:51:16 -0400

    Update data_content.js

--- Revised Webpage/data_content.js
+++ Revised Webpage/data_content.js
@@ -1,7 +1,19 @@
 const data = {
-    recipe: [
+    recipes: [
         {
-            imgFile: "images/data-placeholder.jpg"
+            title: "Chicken Parm",
+            image: "images/data-placeholder.jpg",
+            description: "A classic Italian-American dish with breaded chicken, marinara sauce, and melted cheese."
+        },
+        {
+            title: "Chicken Alfredo",
+            image: "images/data-placeholder.jpg",
+            description: "Creamy and delicious pasta with tender chicken and a rich Alfredo sauce."
+        },
+        {
+            title: "Focaccia Bread",
+            image: "images/data-placeholder.jpg",
+            description: "A flavorful and airy Italian flatbread baked and topped with olive oil."
         }
     ]
-}
+};
